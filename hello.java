cb016f7df511a0f9fddcfb68afda4042f7b0d235
Dev1 <dev1@gmail.com>
2023-08-04 22:57:49 +0530

    file modf

--- hello.java
+++ hello.java
@@ -1,9 +1,9 @@
-class hello()
+class hello()  #from dev1
 
  { 
-	 public static void main();
+	 public static void main(); #from dev1
 	 {
-		 system.out.println("hello new modf");
+		 system.out.println("hello new modf by dev1");
 	 }
 
 }
